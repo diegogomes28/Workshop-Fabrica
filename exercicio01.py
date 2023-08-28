b129c09136999d7919b35d33691ff8b716e3840e
diegogomes28 <diegogomes28282@gmail.com>
2023-08-28 16:58:58 -0300

    Primeira atividade em python na imersão workshop fábrica

---
+++ exercicio01.py
@@ -0,0 +1,11 @@
+''''
+1. Crie um programa que peça um número e mostre o seu sucessor e antecessor
+'''
+
+numero = int(input("Digite o Numero: "))
+sucessor = int(numero) + 1
+antecessor = int(numero) - 1
+
+print("seu número digitado foi: ", numero)
+print("O Sucessor é: " , sucessor)
+print("O Antecessor é: " , antecessor)
