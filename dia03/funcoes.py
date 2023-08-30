eec0d291d8fa48384a6dbe871e0d08fe265de317
diegogomes28 <autologon@CSED.LAB>
2023-08-30 18:04:41 -0300

    Atividades do terceiro dia do workshop

---
+++ dia03/funcoes.py
@@ -0,0 +1,21 @@
+def soma(x: int | float, y: int | float) -> int | float:
+    '''Soma x+y e retora o resultado'''
+    # resultado = x + y
+    return x + y
+
+def print_subtracao(x: int | float, y: int | float) -> None:
+    print(x-y)
+
+def soma_sem_parametro() -> int | float:
+    x = 5
+    y = 5
+    return x + y
+
+"""
+a = soma (1, 2)
+b = print_subtracao(30, 25)
+
+
+print(a)
+print(b)
+"""
---
+++ dia03/main.py
@@ -0,0 +1,6 @@
+from funcoes import soma, print_subtracao
+
+
+print(soma(33, 18))
+
+print_subtracao(1222, 845)
