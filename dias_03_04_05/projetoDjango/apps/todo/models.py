62a80dd66c87454f4469a3df8c4fa65d53035f9c
diegogomes28 <autologon@CSED.LAB>
2023-09-01 15:45:19 -0300

    imersao dia 05

--- dias_03_04_05/projetoDjango/apps/todo/models.py
+++ dias_03_04_05/projetoDjango/apps/todo/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+from apps.users.models import User
+
 # Create your models here.
 class Task(models.Model):
 
     name = models.CharField(max_length=200) #com limite de caract
+    
     description = models.TextField() #sem limites
 
     date = models.DateField()
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.name
